9054fd33e10aeedad3527534ad8fec666e6556ce
Mate <mate@localhost.hu>
2023-10-02 12:51:17 +0200

    Add project files.

---
+++ CA231002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mate";
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
